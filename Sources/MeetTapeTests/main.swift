acc9211fac33898f83ab145553fe4647a882ff1e
andrew <andrew.neeser@me.com>
2026-08-18 20:45:23 -0400

    Build the test suite list by appending
    A single chained + expression across twelve suites exceeded the type
    checker's budget on the toolchain CI runs, though it compiled locally.

--- Sources/MeetTapeTests/main.swift
+++ Sources/MeetTapeTests/main.swift
@@ -1,14 +1,21 @@
 import Foundation
 import TestKit
 
-let suites: [Suite] = CaptureRecoveryTests.all
-    + [ManifestTests.suite, StorageTests.suite, AudioTests.suite]
-    + DetectionTests.all
-    + SessionTests.all
-    + HardeningTests.all
-    + [UITests.suite]
-    + ProcessingTests.all
-    + [PipelineTests.suite, LiveOpenAITests.suite, LiveEndToEndTests.suite]
+// Built by appending rather than by chaining `+`: a single chained expression of
+// this length exceeds the type checker's budget on the toolchain CI runs.
+var suites: [Suite] = []
+suites += CaptureRecoveryTests.all
+suites.append(ManifestTests.suite)
+suites.append(StorageTests.suite)
+suites.append(AudioTests.suite)
+suites += DetectionTests.all
+suites += SessionTests.all
+suites += HardeningTests.all
+suites.append(UITests.suite)
+suites += ProcessingTests.all
+suites.append(PipelineTests.suite)
+suites.append(LiveOpenAITests.suite)
+suites.append(LiveEndToEndTests.suite)
 
 let code = await TestRunner.run(suites, arguments: Array(CommandLine.arguments.dropFirst()))
 exit(code)
